8d56039fc9a5fa8ffb1b40a614729a5f8fc9e65a
taj <thomas.johnson@oregonstate.edu>
2011-07-07 11:39:17 -0700

    added tests and renamed to pynoid

--- pynoid.py
+++ pynoid.py
@@ -1,16 +1,30 @@
 from random import randint
 
 DIGIT = ['0','1','2','3','4','5','6','7','8','9']
-XDIGIT = DIGIT + ['b','c','d','f','g',
-          'h','j','k','m','n','p','q','r','s','t','v','w','x','z']
+XDIGIT = DIGIT + ['b','c','d','f','g','h','j','k','m','n','p','q','r','s','t','v','w','x','z']
 GENTYPES = {'r': 'random', 's': 'sequential', 'z': 'sequential'}
 VERSION = 'pynoid 0.1'
 
 
-def mint(template, n=None, scheme=None, naan=None):
-    # template is of form 'prefix.mask' where prefix is any string 
-    # and mask is a string of any combination 'e' and 'd',
-    # optionally beginning with [r|s|z] and/or ending with 'k'
+def mint(template='zek', n=None, scheme=None, naa=None):
+    ''' Mint identifiers according to template with a prefix of scheme + naa.
+
+    Template is of form [mask] or [prefix].[mask] where prefix is any URI-safe string and mask is a string of any combination 'e' and 'd', optionally beginning with a mint order indicator ('r'|'s'|'z') and/or ending with a checkdigit ('k'):
+    
+    Example Templates:
+    d      : 0, 1, 2, 3
+    zek    : 00, xt, 3f0, 338bh
+    123.zek: 123.00, 123.xt, 123.3f0, 123.338bh
+    seddee : 00000, k50gh, 637qg
+    seddeek: 000000, k06178, b661qj
+
+    The result is appended to the scheme and naa as follows: scheme + naa + '/' + [id].
+
+    There is no checking to ensure ids are not reminted. Instead, minting can be controlled by supplying a (int) value for 'n'. It is possible to implement ordered or random minting from available ids by manipulating this number from another program. If no 'n' is given, minting is random from within the namespace. A ':shrt:' is added between '/' and [id] to indicate that these ids are for short term testing only. An override may be added later to accommodate applications which don't mind getting used ids. 
+
+    A note about 'r', 's', and 'z': 'z' indicates that a namespace should expand on its first element to accommodate any 'n' value (eg. 'de' becomes 'dde' then 'ddde' as numbers get larger). That expansion can be handled by this method. 'r' and 's' (typically meaning 'random' and 'sequential') are recognized as valid values, but ignored and must be implemented elsewhere.
+    '''
+
     if '.' in template:
         prefix, mask = template.rsplit('.', 1)
     else:
@@ -22,27 +36,32 @@ def mint(template, n=None, scheme=None, naan=None):
     except:
         raise
 
-    if not n:
+    if n == None:
         if mask[0] in GENTYPES:
             prefix = ':shrt:' + prefix
             mask = mask[1:]
-        n = randint(0, _getTotal(mask))
+        n = randint(0, _getTotal(mask) -1)
 
     noid = prefix + _n2xdig(n, mask)
+    if naa:
+        noid = naa.strip('/') + '/' + noid
     if template[-1] == 'k':
         noid += _checkdigit(noid)
-
-    if naan:
-        noid = naan.strip('/') + noid
     if scheme:
         noid = scheme + noid
 
     return noid
 
 
-def validate(noid):
-    if not _checkdigit(noid[0:-1]) == noid[-1]:
-        raise ValidationError("Noid check character '" + noid[-1] + "' doesn't match up for '" + noid + "'.")
+def validate(s):
+    '''Checks if the final character is a valid checkdigit for the id. Will fail for ids with no checkdigit.
+
+    This will also attempt to strip scheme strings (eg. 'doi:', 'ark:/') from the beginning of the string. This feature is limited, however, so if you haven't tested with your particular namespace, it's best to pass in ids with that data removed.
+
+    Returns True on success, ValidationError on failure.
+    '''
+    if not _checkdigit(s[0:-1]) == s[-1]:
+        raise ValidationError("Noid check character '" + s[-1] + "' doesn't match up for '" + s + "'.")
     return True
 
 
@@ -111,6 +130,12 @@ def _getTotal(mask):
         
 
 def _checkdigit(s):
+    # TODO: Fix checkdigit to autostrip scheme names shorter or longer than 3 chars.
+    try:
+        if s[3] == ':':
+            s = s[4:].lstrip('/')
+    except IndexError:
+        pass
     def ordinal(x):
         try: return XDIGIT.index(x)
         except: return 0
---
+++ test.py
@@ -0,0 +1,61 @@
+
+import unittest
+import pynoid
+
+class PynoidTests(unittest.TestCase):
+    
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_naa_append(self):
+        noid = pynoid.mint(naa='abc')
+        self.assertTrue(noid.startswith('abc/'))
+
+    def test_scheme_append(self):
+        schemes = ['doi:', 'ark:/']
+        for scheme in schemes:
+            noid = pynoid.mint(scheme=scheme)
+            self.assertTrue(noid.startswith(scheme))
+    
+    def test_mint_short_term(self):
+        noid = pynoid.mint()
+        self.assertTrue(noid.startswith(':shrt:'))
+
+    def test_mint_ns(self):
+        ns = range(10)
+        for n in ns:
+            self.assertEqual(pynoid.mint('d', n), pynoid.DIGIT[n])
+        ns = range(29)
+        for n in ns:
+            self.assertEqual(pynoid.mint('e', n), pynoid.XDIGIT[n])
+
+    def test_namespace_overflow(self):
+        self.assertRaises(pynoid.NamespaceError, pynoid.mint, template='d', n=10)
+        self.assertRaises(pynoid.NamespaceError, pynoid.mint, template='e', n=29)
+
+    def test_mint_z_rollover(self):
+        self.assertEqual(pynoid.mint('zd', 10), '10')        
+        self.assertEqual(pynoid.mint('ze', 29), '10')
+
+    def test_validate_valid(self):
+        valid = 'test31wqw0wsr'
+        validScheme = 'ark:/test31wqw0wsr'
+        self.assertTrue(pynoid.validate(valid))
+        self.assertTrue(pynoid.validate(validScheme))
+
+    def test_validate_invalid(self):
+        invalid = 'test31qww0wsr'
+        invalidScheme = 'ark:/test31qww0wsr'
+        self.assertRaises(pynoid.ValidationError, pynoid.validate, invalid)
+        self.assertRaises(pynoid.ValidationError, pynoid.validate, invalidScheme)
+
+    def test_checkdigit(self):
+        self.assertEqual(pynoid.mint('eek', 100), '3f0')
+        self.assertRaises(pynoid.ValidationError, pynoid.validate, 'f30')
+
+if __name__ == '__main__':
+    unittest.main()
+        
